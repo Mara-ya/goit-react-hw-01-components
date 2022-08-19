f4f8a5eeebea6fc265db36359702a9680d4dbf63
Mara-ya <97354812+Mara-ya@users.noreply.github.com>
2022-08-19 23:19:42 +0300

    Update FriendList.js

--- src/components/FriendListItem/FriendList.js
+++ src/components/FriendListItem/FriendList.js
@@ -4,13 +4,9 @@ import { OneFriend, FriendStatus, FriendPhoto, FriendName } from './FriendListIt
 export const FriendListItem = ({ isOnline, avatar, name}) => {
     return (
         <OneFriend>
-            <FriendStatus style={{ background: `${friendStatus(isOnline)}` }}>{isOnline}</FriendStatus>
+            <FriendStatus isOnline={isOnline}></FriendStatus>
             <FriendPhoto src={avatar} alt="User avatar" width="48" />
             <FriendName>{name}</FriendName>
         </OneFriend>
     )
 }
-
-function friendStatus(props) {
-    return props ? 'green' : 'red';
-  }
